7d5c279eb59611c6e4211bfea2a064c4472620ce
Kim Love <kimberlymlove15@gmail.com>
2023-12-13 17:50:26 -0500

    Created a smaller navbar for smaller screens

---
+++ client/src/components/NavBarSmallScreen.js
@@ -0,0 +1,50 @@
+import { NavLink } from "react-router-dom";
+import "../CSS/navBarStyles.css"
+import { Avatar } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+
+function NavBarSmallScreen({user, handleSetFilterScheduleEventsValue, handleFutureEventsCheck, handleCloseNavBar}) {
+    // const {player_name, coach_name, headshot_img_url} = user
+
+  return (
+    <div className="navBarContainerSmallScreen">
+
+            {/* {headshot_img_url? 
+                <div id="headshot-container">
+                    <img 
+                        src={headshot_img_url}
+                        alt={`${coach_name? coach_name : player_name} headshot`}
+                    />
+                </div> 
+                : 
+                <Avatar size={125} icon={<UserOutlined />} />
+            } */}
+            
+            {/* <h3>{coach_name ? coach_name : player_name}</h3> */}
+            <nav className="navBar">
+                <NavLink onClick={() => {
+                    handleSetFilterScheduleEventsValue(0)
+                    handleFutureEventsCheck(true)
+                    handleCloseNavBar()
+                }} to="/home">Home</NavLink>
+                <NavLink onClick={() => {
+                    handleSetFilterScheduleEventsValue(0)
+                    handleFutureEventsCheck(true)
+                    handleCloseNavBar()
+                }} to="/teams">Teams</NavLink>
+                <NavLink onClick={() => {
+                    handleSetFilterScheduleEventsValue(0)
+                    handleFutureEventsCheck(true)
+                    handleCloseNavBar()
+                }} to="/schedule">Schedule</NavLink>
+                <NavLink onClick={() => {
+                    handleSetFilterScheduleEventsValue(0)
+                    handleFutureEventsCheck(true)
+                    handleCloseNavBar()
+                }} to="/account">Account</NavLink>
+            </nav>
+        </div>               
+  )
+}
+
+export default NavBarSmallScreen
